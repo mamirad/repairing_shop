ddc479156e9ab4f8a2d6a152186d91f07ec7a39b
Waleed Asghar <waleed.asghar@transreport.co.uk>
2022-12-27 12:24:21 +0500

    require iemi false

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
 	belongs_to :customer
 	belongs_to :refer_source, optional: true
 	enum status: [:at_shop, :repairing, :repaired, :delivered ]
-	validates :name, :model, :brand,:iemi, presence: true
+	validates :name, :model, :brand, presence: true
 end
